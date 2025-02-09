a404ea5f7c8e31b40455f8d3dc9d486393a17345
2pizzzza <amit.kurbanov.123.com@gmail.com>
2025-02-09 12:48:07 +0600

    Feat: file:struct, and arch

--- cmd/sentinel/main.go
+++ cmd/sentinel/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/2pizzzza/sentinetAgent/internal/config"
+)
+
 func main() {
 
+	config, err := config.New("config/config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*config)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		DBName   string `yaml:"dbname"`
+	} `yaml:"database"`
+}
+
+func New(path string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = yaml.Unmarshal(file, config)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing config file: %w", err)
+	}
+
+	return config, nil
+}
